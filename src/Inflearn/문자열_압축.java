f8e8da76c7ddcd213bc5e9fd8780dd0eac0eda7b
bb9oo <hyeonjubb@gmail.com>
2022-12-25 16:47:54 +0900

    문자열 압축

--- src/Inflearn/문자열_압축.java
+++ src/Inflearn/문자열_압축.java
@@ -1,5 +1,43 @@
 package Inflearn;
 
-public class 문자열_압축 {
+import java.util.Scanner;
 
+public class 문자열_압축 {
+	public String solution(String s) {
+		
+		String answer = "";
+		s = s+" "; //맨끝에 빈문자 하나 추가해주기!
+		
+		int cnt =1;
+		
+		for(int i = 0; i< s.length(); i++) { // 빈문자전까지만 가게하기 위해서 -1
+			if (s.charAt(i)==s.charAt(i+1)) cnt++;
+			else {
+				answer += s.charAt(i);
+				
+				//cnt가 1보다 크다면 answer에 
+				//cnt를 String으로 변환 후 더하기
+				//String.valueOf() 또는 Integer.toString(cnt) 사용 
+				if(cnt>1) answer+= String.valueOf(cnt);
+				cnt = 1; // 다시 cnt는 1로 초기화 
+				
+			}
+		}
+		
+		
+		return answer;
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		문자열_압축 T = new 문자열_압축();
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		System.out.println(T.solution(str));
+		
+	}
 }
