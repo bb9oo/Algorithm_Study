fe0b56e4212c9aee61baf715a5b299ec2bc01840
bb9oo <hyeonjubb@gmail.com>
2022-12-31 14:12:17 +0900

    코드 정리

--- src/Inflearn/대소문자_변환.java
+++ src/Inflearn/대소문자_변환.java
@@ -8,11 +8,12 @@ public class 대소문자_변환 {
 		String  answer = "";
 		
 		for(char x : str.toCharArray()) {
-			/*
-			 * //x가 소문자라면 answer에 x를 대문자로 바꾼것을 누적 if(Character.isLowerCase(x))
-			 * answer+=Character.toUpperCase(x); //x가 대문자라면 answer에 x를 소문자로 바꾼것을 누적 else
-			 * answer+=Character.toLowerCase(x);
-			 */
+			
+//			//다른 풀이 방법
+//			//x가 소문자라면 answer에 x를 대문자로 바꾼것을 누적 
+//			if(Character.isLowerCase(x)) answer+=Character.toUpperCase(x); //x가 대문자라면 answer에 x를 소문자로 바꾼것을 누적 else
+//			answer+=Character.toLowerCase(x);
+			 
 			
 			//아스키 넘버로 푸는 방법
 			if(x>=97 && x<=122) answer+=(char)(x-32);//소문자라면 대문자로 변환(대소문자 차이 = 32)
