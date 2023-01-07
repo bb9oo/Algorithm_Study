b3acc3ee24e6c0a0a0fa403e80ea9085ce89f659
Hyeon Ju <109609187+bb9oo@users.noreply.github.com>
2023-01-07 16:02:15 +0900

    [Bronze V] Title: A+B - 7, Time: 260 ms, Memory: 18656 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		int[] arr= new int[a];
+		
+
+		for (int i = 1; i <= a; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+ 
+			System.out.println("Case #" + i + ": " + (A+B));
+		}
+ 
+		
+	}
+}
