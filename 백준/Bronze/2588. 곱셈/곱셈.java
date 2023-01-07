306e6ebb41bffea3ea4a50ed812808f3622e5184
Hyeon Ju <109609187+bb9oo@users.noreply.github.com>
2023-01-07 15:42:26 +0900

    [Bronze III] Title: 곱셈, Time: 204 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int num3 = a * (b%10);
+		int num4 = a * ((b/10)%10);
+		int num5 = a * ((b/100)%10);
+		int num6 = num3+(num4*10)+(num5*100);
+		
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+		System.out.println(num6);
+		
+	}
+}
